89fa9a06369fae1fc0a2531176b49845b028adbb
Alexus91 <mrfadlkamal@gmail.com>
2024-05-26 05:39:26 +0100

    the for loop

---
+++ forloop.java
@@ -0,0 +1,9 @@
+/*the syntax for a for loop is 
+for (statement 1; statement 2; statement 3) { code block to be executed } */
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }    
+}
