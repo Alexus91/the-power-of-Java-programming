1780018915fea070537cdb2a2a53302c4f164fcd
Alexus91 <mrfadlkamal@gmail.com>
2024-05-26 04:17:41 +0100

    if statement eles ...

---
+++ Ifstatement.java
@@ -0,0 +1,11 @@
+public class Ifstatement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a > b) { // the condition is false so it will not print the statement
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("b is greater than a");
+        }
+    }
+}
