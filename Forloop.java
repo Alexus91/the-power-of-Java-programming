b4807c26d991b77d85d2c3777fe8cb5a5099dfa7
Alexus91 <mrfadlkamal@gmail.com>
2024-05-26 05:55:59 +0100

    while loop

--- Forloop.java
+++ Forloop.java
@@ -1,6 +1,6 @@
 /*the syntax for a for loop is 
 for (statement 1; statement 2; statement 3) { code block to be executed } */
-public class forloop {
+public class Forloop {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
---
+++ Whileloop.java
@@ -0,0 +1,10 @@
+/* while i is lower than the loop will keep run and print the value i  */
+public class Whileloop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
