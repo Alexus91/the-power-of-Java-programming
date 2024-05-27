f85703a9c404deeefa235b15e391fdf9a39f7591
Alexus91 <mrfadlkamal@gmail.com>
2024-05-27 10:20:33 +0100

    the first 2 types of the functions 1- no return and no args 2- return with no args

---
+++ Typeonefunction.java
@@ -0,0 +1,14 @@
+public class Typeonefunction {
+    // type one function is a function with no return type and no parameters
+    
+    
+    static void addnumbers() {
+        // this function adds two numbers the void is the return type means no return   or NULL
+        int a = 5;
+        int b = 10;
+        System.out.println(a + b);}
+    
+    public static void main( String[] args) {
+        addnumbers();}
+    
+}
---
+++ Typetowfunction.java
@@ -0,0 +1,11 @@
+public class Typetowfunction {
+    // Function with return type and no arguments
+    static String myname() {
+        String name = "John Doe";
+        // this function returns a string
+        return name;
+    }
+    public static void main(String[] args) {
+        System.out.println(myname());
+    }
+}
