207087277a3be3b04dffd20142c27bb8bf8e0cb0
Alexus91 <mrfadlkamal@gmail.com>
2024-05-27 11:13:51 +0100

    type 3- no return and arguments type 4- function with return and arguments

---
+++ Typeforthfun.java
@@ -0,0 +1,10 @@
+public class Typeforthfun {
+    // Function with return type and arguments
+    static int addnumbers(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println(addnumbers(5, 10));
+    }
+    
+}
---
+++ Typethreefun.java
@@ -0,0 +1,10 @@
+public class Typethreefun {
+    // Function with no return type and arguments
+    static void getmyname(String name) {
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        getmyname("kamal fadl");
+    }   
+}
+
