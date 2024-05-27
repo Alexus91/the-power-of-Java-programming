8b4e0ff92e11ef06c5a4eabdab8d6dd357ead7e9
Alexus91 <mrfadlkamal@gmail.com>
2024-05-27 09:47:16 +0100

    how functions works and types

---
+++ Functionandtypes.java
@@ -0,0 +1,24 @@
+
+public class  Functionandtypes {
+    // data type(return type) function name(parameters)
+    public static void myFunction() {
+        //write something here
+        System.out.println("This function works");
+    }
+    static void printsum(int a, int b) {
+        System.out.println(a + b);
+    }
+    public static void main(String[] args) {
+        myFunction();
+        printsum(5, 10);
+        
+    }
+    // types of functions:
+    // 1. Function with no return type and no parameters
+    // 2. Function with no return type and parameters
+    // 3. Function with return type and no parameters
+    // 4. Function with return type and parameters
+    // etc ...
+
+    
+}
